11062bc3e323c4382d1e86a4cf936991eb5e3d27
kllza a <carmartinez9901@gmail.com>
2025-03-27 12:55:28 +0000

    SCP1.2

--- myApp.js
+++ myApp.js
@@ -15,7 +15,8 @@ app.use(helmet.dnsPrefetchControl())
 app.use(helmet.noCache())
 app.use(helmet.contentSecurityPolicy({
   directives: {
-    defaultSrc: ["'self'", 'trusted-cdn.com' ],
+    defaultSrc: ["'self'" ],
+    scriptSrc: ["'self'", 'trusted-cdn.com']
   }
 }));
 
